58956d4cae4923fa5ab78e1dedf5f86771507291
Marty Chao <marty@browan.com>
2018-10-14 20:50:44 +0800

    format for PEP8

--- Lab00_MQTT_dummy_test.py
+++ Lab00_MQTT_dummy_test.py
@@ -1,14 +1,15 @@
 #! /usr/bin/python3
 # -*- coding: utf8 -*-
-# dummy 就是不用模組也可驗證MQTT 連線的狀況，credentials 範例裏的 IP, account 是會固定吐出MQTT 資訊的。
+# dummy 就是不用模組也可驗證MQTT 連線的狀況，範例裏的 IP, account 是會固定吐出MQTT 資訊的。
 __author__ = "Marty Chao"
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 __maintainer__ = "Marty Chao"
 __email__ = "marty@browan.com"
 __status__ = "Production"
 # Change Log 1.0.3 , support paho-mqtt v1.2
 # Change Log 1.0.4 , 優化顯示訊息
 # Change Log 2.0.1 , Python 3 version
+# Change Log 2.0.2 , format for PEP8
 
 try:
     import paho.mqtt.client as mqtt
@@ -17,11 +18,8 @@ except ImportError:
     print("cmd:pip3 install paho-mqtt")
     exit()
 import json
-# 下方資料是自動吐出json資料，不需要模組發送就會有資料
-#HostName = "52.193.146.103"
 HostName = "mqtt.lazyengineers.com"
 PortNumber = 1883
-#Topic = "client/200000017/200000017-GIOT-MAKER"
 Topic = "GIOT-GW/#"
 UserName = "lazyengineers"
 Password = "lazyengineers"
@@ -42,7 +40,7 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, msg):
-#    print(msg.topic+" "+str(msg.payload))
+    # print(msg.topic+" "+str(msg.payload))
     json_data = msg.payload
 #   print(json_data)
     sensor_data = json.loads(json_data)[0]['data']
@@ -50,10 +48,13 @@ def on_message(client, userdata, msg):
     sensor_time = json.loads(json_data)[0]['time']
     date_value = sensor_time.split("T")[0]
     time_value = sensor_time.split("T")[1]
-    if (sensor_macaddr == MacAddr) or (MacAddr == "00000000") :
+    if (sensor_macaddr == MacAddr) or (MacAddr == "00000000"):
         print("Topic:" + msg.topic)
         print(json_data)
-        print("date:" + date_value + ", time:" + time_value + " AT+DTX payload:" + sensor_data)
+        print(
+            "date:" + date_value
+            + ", time:" + time_value
+            + " AT+DTX payload:" + sensor_data)
 
 
 client = mqtt.Client(protocol=mqtt.MQTTv31)
@@ -61,7 +62,7 @@ client.on_connect = on_connect
 client.on_message = on_message
 client.username_pw_set(UserName, Password)
 
-#client.connect(HostName, PortNumber, 60)
+# client.connect(HostName, PortNumber, 60)
 client.connect(HostName, PortNumber, 60)
 
 # Blocking call that processes network traffic, dispatches callbacks and
