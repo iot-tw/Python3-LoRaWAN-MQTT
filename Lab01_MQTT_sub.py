c73890a480fd2e3c0ea7c0e9a9745acadc3266d9
Marty Chao <marty@browan.com>
2018-10-14 20:53:55 +0800

    format for PEP8

--- Lab01_MQTT_sub.py
+++ Lab01_MQTT_sub.py
@@ -2,16 +2,15 @@
 # -*- coding: utf8 -*-
 # 搭配 LAB01 Arduino 的上傳資料 AT_DTX Raw Data
 __author__ = "Marty Chao"
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 __maintainer__ = "Marty Chao"
 __email__ = "marty@browan.com"
 __status__ = "Production"
 # Change log 1.0.2, support paho-mqtt 1.2
 # Change log 1.0.3, 去掉credentials 機制
+# Change Log 1.0.4, clean useless comment and format for PEP8
 
 import paho.mqtt.client as mqtt
-import json
-#HostName = "52.193.146.103"
 HostName = "mqtt.lazyengineers.com"
 PortNumber = 1883
 Topic = "GIOT-GW/#"
@@ -33,19 +32,6 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
     json_data = msg.payload
-    # sensor_data = json.loads(json_data)['data']
-    # 處理MQTT 抓下來的 資料json 中的 data 欄位，用hex decode 回來
-    # sensor_value = sensor_data.decode("hex")
-    # 抓取json 中的 gwid 這是表示通過那個AP 送進來的
-    # gwid_data = json.loads(json_data)['extra']['gwid']
-    # 抓取模組的MacAddr
-    # sensor_macAddr = json.loads(json_data)['macAddr']
-    # 過濾某一個特定GIoT AP 送進來的 MQTT 資料，其他的不要
-    # 每個 Indoor AP, OutDoor AP 都有兩個gwid,所以要抓兩個進來,如果不考慮過濾可以註釋掉
-    # if gwid_data == "00001c497b48dc03" or gwid_data == "00001c497b48dc11":
-#    if sensor_macAddr == macAddr:
-#       print('ID: ' + macAddr)
-#       print('AT ASCII value: ' + sensor_value)
     print(json_data)
 
 
