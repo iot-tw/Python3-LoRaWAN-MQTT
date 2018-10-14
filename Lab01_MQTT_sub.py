c17387962d2ac82744a18ba0d0067d18a86e252d
Marty Chao <marty@browan.com>
2018-10-14 20:54:31 +0800

    format for PEP8

--- Lab01_MQTT_sub.py
+++ Lab01_MQTT_sub.py
@@ -30,7 +30,7 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    print(msg.topic + " " + str(msg.payload))
     json_data = msg.payload
     print(json_data)
 
